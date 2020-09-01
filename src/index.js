a499dc3624a91111dc5bf1fe6133e877121079f7
StephSince93 <stephen@stemsoftware.com>
2020-09-01 14:16:01 -0500

    Updaes with sock.io, stuff working now

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-const express = require('express')
 const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
+let count = 0
 
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+    console.log('New Websocket connection!')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        //socket.emit('countUpdated', count) //Only emits to a specific client connection
+        io.emit('countUpdated', count) //Emits to all client connections
+    })
+})
+
+server.listen(port, () => {
     console.log(`Listening on ${port}`)
 })
