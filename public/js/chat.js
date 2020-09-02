823dc09320c4c0e141bb33875aa14626c3a64b93
StephSince93 <stephen@stemsoftware.com>
2020-09-02 09:23:02 -0500

    cleaned up js variables inside chat.js file

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,13 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#textForm')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+const $locationButton = document.querySelector('#send-location')
+
+
 socket.on('location', (location) => {
         console.log(location)
         //document.querySelector('#title').textContent = message
@@ -12,11 +20,19 @@ socket.on('message', (message) => {
 
 })
 
-document.querySelector('#textForm').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
         e.preventDefault()
+
+        $messageFormButton.setAttribute('disabled', 'disabled')
+
         const message = e.target.elements.message.value
 
         socket.emit('sendMessage', message, (error) => {
+
+            $messageFormButton.removeAttribute('disabled')
+            $messageFormInput.value = ''
+            $messageFormInput.focus()
+
             if(error) {
                 return console.log(error)
             }
@@ -24,17 +40,20 @@ document.querySelector('#textForm').addEventListener('submit', (e) => {
         })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
-    
+$locationButton.addEventListener('click', () => {
+
     if(!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+
+    $locationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude : position.coords.latitude,
             longitude : position.coords.longitude
         }, (message) => {
+            $locationButton.removeAttribute('disabled')
             console.log(message)
 
         })
